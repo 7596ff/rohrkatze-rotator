c04263365fa9b6a2a6f8640a8cb8213b5b19ee7b
Samantha McCarthy <1760581+7596ff@users.noreply.github.com>
2019-02-15 10:48:38 -0500

    add changeavy command

---
+++ commands/changeavy.js
@@ -0,0 +1,18 @@
+const request = require("snekfetch");
+
+async function exec(message, ctx) {
+    let response = await snekfetch.get(ctx.content);
+    await ctx.client.bot.editSelf( { avatar: "data:image/jpg;base64," + response.body.toString("base64") });
+}
+
+async function checks(member, ctx) {
+    return member.id == ctx.client.options.owner;
+}
+
+module.exports = {
+    name: "changeavy",
+    category: "owner",
+    immune: true,
+    checks,
+    exec
+};
